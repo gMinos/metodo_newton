79da9d447bf975e1d7f9b468613a9048f1a19c52
gMinos <gjobelarmino@gmail.com>
2024-11-29 20:49:56 -0500

    refactorizando
    - condicion para terminar el bucle y dar respuesta

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,12 @@ fn determinar_funcion(y: &String, x: &f64) -> f64 {
     funcion(*x)
 }
 
-fn derivada_metodo_numerico(f: &f64, y: &String, h: &f64) -> f64 {
-    let f_x_h: f64 = determinar_funcion(&y, h);
+fn derivada_metodo_numerico(f: &f64, f_x_h: &f64) -> f64 {
     (f_x_h - *f) / H
 }
 
-fn metodo_newton(x: &mut f64, f: &f64, f_prima: &f64) -> f64 {
-    *x - (f / f_prima)
+fn metodo_newton(x: &mut f64, f: &f64, f_prima: &f64) {
+    *x -= f / f_prima
 }
 
 fn main() {
@@ -32,17 +31,23 @@ fn main() {
         .parse()
         .expect("Error al convertir a valor numerico");
 
-    println!("digite la funcion. \n- ejemplo : 2*x^3 + 5*x * (2*x - 1)^3 + x^2");
+    println!("digite la funcion. \n- ejemplo : 2*x^3 + 5 * (2*x - 1)^3 + x^2");
     let y: String = leer_valores();
-    let mut h: f64;
+    let mut f_x_h: f64;
     let mut f: f64;
     let mut f_prima: f64;
+    let mut cmp: f64;
 
-    for _ in 0..9 {
-        h = x + H;
+    loop {
+        cmp = x;
+        f_x_h = determinar_funcion(&y, &(x + H));
         f = determinar_funcion(&y, &x);
-        f_prima = derivada_metodo_numerico(&f, &y, &h);
-        x = metodo_newton(&mut x, &f, &f_prima);
+        f_prima = derivada_metodo_numerico(&f, &f_x_h);
+        metodo_newton(&mut x, &f, &f_prima);
+
+        if cmp.eq(&x) {
+            break;
+        }
     }
 
     println!("resultado = {x}");
